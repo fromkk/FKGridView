7459b7723102f95adbae74b8aa0ab4004e0d2a33
fromkk <info@fromkk.info>
2014-03-02 18:29:37 +0900

    Initial Commit

---
+++ fkGridView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  fkGridView
+//
+//  Created by Ueoka Kazuya on 2014/03/02.
+//  Copyright (c) 2014年 fromKK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ fkGridViewTests/fkGridViewTests.m
@@ -0,0 +1,34 @@
+//
+//  fkGridViewTests.m
+//  fkGridViewTests
+//
+//  Created by Ueoka Kazuya on 2014/03/02.
+//  Copyright (c) 2014年 fromKK. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface fkGridViewTests : XCTestCase
+
+@end
+
+@implementation fkGridViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
